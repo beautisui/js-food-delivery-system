9528571f44006546e86036e008ccc3a546591abc
Beauti <183709979+beautisui@users.noreply.github.com>
2025-01-31 15:04:53 +0530

    add time diffrences

--- food_delivery_system/main.js
+++ food_delivery_system/main.js
@@ -1,35 +1,60 @@
-const deliveryDetails = (orderStatus) => {
-  console.log("Delivering order...");
+const timeElapsed = (startTime, currentTime) => {
+  const timeTaken = (currentTime - startTime) / 1000;
+  return `[${timeTaken.toFixed(2)}]s`;
+};
+
+const deliveryDetails = (orderStatus, initialTime) => {
+  console.log(timeElapsed(initialTime, Date.now()), "Delivering order...");
   setTimeout(() => {
     orderStatus.deliveryDetails = "Delivered by John at 7:30 PM";
-    console.log("Order delivered :", orderStatus);
+    console.log(
+      timeElapsed(initialTime, Date.now()),
+      "Order delivered :",
+      orderStatus,
+    );
   }, 5000);
 };
 
-const packOrder = (orderStatus) => {
-  console.log("Packing order...");
+const packOrder = (orderStatus, initialTime) => {
+  console.log(timeElapsed(initialTime, Date.now()), "Packing order...");
   setTimeout(() => {
     orderStatus.packageDetails = "Packed in eco-friendly box";
-    console.log(" Order packed:", orderStatus);
-    deliveryDetails(orderStatus);
+    console.log(
+      timeElapsed(initialTime, Date.now()),
+      " Order packed:",
+      orderStatus,
+    );
+    deliveryDetails(orderStatus, initialTime);
   }, 2000);
 };
 
-const prepareFood = (orderStatus, menu) => {
-  console.log("Preparing food...");
+const prepareFood = (orderStatus, menu, initialTime) => {
+  const currentTime = Date.now();
+  console.log(timeElapsed(initialTime, currentTime), "Preparing food...");
+
   setTimeout(() => {
     orderStatus.foodDetails = menu;
-    console.log("Food is ready:", orderStatus);
-    packOrder(orderStatus);
+    console.log(
+      timeElapsed(initialTime, Date.now()),
+      "Food is ready:",
+      orderStatus,
+    );
+    packOrder(orderStatus, initialTime);
   }, 3000);
 };
 
 const orderReceived = (menu) => {
+  const initialTime = Date.now();
   const orderStatus = {};
   const customerID = 123;
   orderStatus.customerID = customerID;
-  console.log("Order received :", orderStatus);
-  prepareFood(orderStatus, menu);
+
+  console.log(
+    timeElapsed(initialTime, initialTime),
+    "Order received :",
+    orderStatus,
+  );
+  prepareFood(orderStatus, menu, initialTime);
 };
 
 const main = () => {
